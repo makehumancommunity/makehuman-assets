78fa4934a9972eb94563d2ccfb03add5adea0d25
Joel Palmius <joepal1976@hotmail.com>
2020-09-26 10:53:37 +0200

    Cleanup and alternative directory (for debug)

--- license_headers/replaceHeaders.pl
+++ license_headers/replaceHeaders.pl
@@ -3,13 +3,19 @@
 $extension = $ARGV[0];
 chomp($extension);
 
+$dir = $ARGV[1];
+chomp($dir);
+if (!$dir) {
+  $dir = "../base";
+}
+
 if(!$extension) {
   die "Must provide extension\n";
 }
 
 print "Updating files with extension '$extension'\n";
 
-open(PIPE, "find ../base -name \"*.$extension\" |") || die "Could not open pipe\n\n";
+open(PIPE, "find $dir -name \"*.$extension\" |") || die "Could not open pipe\n\n";
 
 while($inlin = <PIPE>) {
   chomp($inlin);
@@ -17,6 +23,7 @@ while($inlin = <PIPE>) {
   system "cat $inlin | grep -v \"#\" > $inlin.nocomment";
   system "cp -f $extension.txt $inlin";
   system "cat $inlin.nocomment >> $inlin";
+  system "rm -f $inlin.nocomment";
 }
 
 close(PIPE);
